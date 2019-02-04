b565aad0ce7ca7dd507020760e0fe6a66142f8d5
Matheus Valeriano <math.valeriano@gmail.com>
2019-02-04 18:46:51 -0200

    feat: add css reset

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<div />, document.getElementById('root'));
