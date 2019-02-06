801ba65aff9575011bac4659e91289ef55e16265
Matheus Valeriano <math.valeriano@gmail.com>
2019-02-05 23:35:36 -0200

    refactor: use app into index

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import App from './components/App';
 
-ReactDOM.render(<div />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
