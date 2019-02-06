32adfaa828d59a6d7ae61dc98205bab6d6d87306
Matheus Valeriano <math.valeriano@gmail.com>
2019-02-05 23:36:20 -0200

    feat: add api to save hotspots

---
+++ src/api/hotspots.js
@@ -0,0 +1,8 @@
+export const getHotspots = () => localStorage.getItem('hotspots') || '[]';
+
+export const saveHotspots = (hotspots = []) => {
+  localStorage.setItem(
+    'hotspots',
+    JSON.stringify(hotspots)
+  );
+}
