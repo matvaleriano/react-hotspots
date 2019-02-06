f62a6c53f306a4889624bc3b6afd33dcfd49827f
Matheus Valeriano <math.valeriano@gmail.com>
2019-02-05 23:39:39 -0200

    refactor: use components have been created in app component

---
+++ src/components/App/index.js
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react';
+import Context from '../Context';
+import Header from '../Header';
+import Hotspot from '../Hotspot';
+import HotspotAdd from '../HotspotAdd';
+import HotspotsList from '../HotspotsList';
+import useHotspots from '../../hooks/useHotspots';
+
+const App = () => {
+  const hotspots = useHotspots();
+  const { hotspots: markers = [] } = hotspots;
+  const { Provider } = Context;
+
+  return (
+    <Provider value={hotspots}>
+      <Header />
+
+      <HotspotAdd />
+      <HotspotsList />
+
+      { markers.map((hotspot, index) => (
+          <Hotspot
+            key={index.toString()}
+            {...hotspot}
+          />
+        ))
+      }
+    </Provider>
+  );
+};
+
+export default App;
