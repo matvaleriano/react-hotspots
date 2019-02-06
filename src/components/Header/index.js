656fa5aed52787621dcc0363d27002f8a23efbcd
Matheus Valeriano <math.valeriano@gmail.com>
2019-02-05 23:39:11 -0200

    feat: add header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+import StyledHeader from './styled';
+import Nav from '../Nav';
+import NavLink from '../Nav/Link';
+
+const Header = () => (
+  <StyledHeader>
+    <Nav>
+      <NavLink>Fake Link 1</NavLink>
+      <NavLink>Fake Link 2</NavLink>
+      <NavLink>Fake Link 3</NavLink>
+      <NavLink>Fake Link 4</NavLink>
+    </Nav>
+  </StyledHeader>
+);
+
+export default Header;
