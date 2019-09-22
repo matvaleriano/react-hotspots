764f1abac9b011a94f4fa0883f191380f9041ecf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-22 01:21:15 +0000

    Deploy mathvaleriano/react-hotspots to github.com/mathvaleriano/react-hotspots.git:gh-pages

---
+++ precache-manifest.34548fc5edc787567700a5174e14a2e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2730394749c98de732ae",
+    "url": "/react-hotspots/static/css/main.c67654fe.chunk.css"
+  },
+  {
+    "revision": "2730394749c98de732ae",
+    "url": "/react-hotspots/static/js/main.27303947.chunk.js"
+  },
+  {
+    "revision": "054291dedc5a91d29c15",
+    "url": "/react-hotspots/static/js/1.054291de.chunk.js"
+  },
+  {
+    "revision": "787fa6d4f62555dc172f",
+    "url": "/react-hotspots/static/js/runtime~main.787fa6d4.js"
+  },
+  {
+    "revision": "69ada60a1b92d5fa3465c482e14c3e31",
+    "url": "/react-hotspots/index.html"
+  }
+];
