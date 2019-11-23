d7a200fd8896eef9c2fd9a963499a21db45220ec
Matheus Valeriano <math.valeriano@gmail.com>
2019-11-23 14:52:03 -0300

    refactor: add types to useResize
    move it to folder

--- src/hooks/useResize/index.ts
+++ src/hooks/useResize/index.ts
@@ -1,17 +1,20 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
-const useResize = (el = document.body) => {
+const useResize = (el: HTMLElement = document.body): {width: number, height: number} => {
   const [size, setSize] = useState({
     width: el.offsetWidth,
     height: el.offsetHeight,
   });
 
-  const handleResize = () => {
-    setSize({
-      width: el.offsetWidth,
-      height: el.offsetHeight,
-    });
-  };
+  const handleResize = useCallback(
+    (): void => {
+      setSize({
+        width: el.offsetWidth,
+        height: el.offsetHeight,
+      });
+    },
+    [el],
+  );
 
   useEffect(() => {
     window.addEventListener('resize', handleResize);
