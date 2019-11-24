e7f698aa2df7612b666addcd73f74d7438b8ea8b
Matheus Valeriano <math.valeriano@gmail.com>
2019-11-24 19:41:52 -0300

    test: add tests for hotspot behavior

---
+++ src/components/Hotspot/index.test.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  render,
+  fireEvent,
+  cleanup,
+  RenderResult,
+} from '@testing-library/react';
+import { getByTestId } from '@testing-library/dom';
+import Hotspot from '.';
+
+let wrapper: RenderResult;
+
+const baseProps = {
+  id: new Date().toISOString(),
+  position: {
+    left: 0,
+    top: 0,
+  },
+};
+
+beforeEach(() => {
+  wrapper = render(<Hotspot {...baseProps} />);
+});
+
+afterEach(cleanup);
+
+test('should initially show hotspotInfo', () => {
+  const hotspotInfo = getByTestId(wrapper.container, 'hotspotInfo');
+  expect(hotspotInfo).not.toBeNull();
+});
+
+test('should show a spot', () => {
+  const spot = getByTestId(wrapper.container, 'spot');
+  expect(spot).not.toBeNull();
+});
+
+test('should hide hotspotInfo when click on spot', async () => {
+  const spot = getByTestId(wrapper.container, 'spot');
+  fireEvent.click(spot);
+  expect(() => getByTestId(wrapper.container, 'hotspotInfo')).toThrow();
+});
+
+test('should initially hide hotspotInfos', () => {
+  const { container, rerender } = wrapper;
+  rerender(<Hotspot {...baseProps} title="title" description="description" />);
+  expect(() => getByTestId(container, 'hotspotInfo')).toThrow();
+});
