5c9c4e08f892a4cd5ad60714586e0b6b597fda4f
Matheus Valeriano <math.valeriano@gmail.com>
2019-11-24 13:19:50 -0300

    feat: add default values to context

--- src/components/Context/index.tsx
+++ src/components/Context/index.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { Hotspot } from 'shared/types/hotspot';
+import { UseHotspotsResult } from 'hooks/useHotspots/types';
 
-const Context = React.createContext();
+const Context = React.createContext<UseHotspotsResult>({
+  state: {
+    isPointing: false,
+    hotspots: [],
+  },
+  actions: {
+    deleteHotspot: (id: string): void => {},
+    editHotspot: (hotspot: Hotspot): void => {},
+    saveHotspot: (hotspot: Hotspot): void => {},
+    toggleIsPointing: (isPointing: boolean): void => {},
+  },
+});
 
 export default Context;
