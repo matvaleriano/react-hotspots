0276e1b2472a08b00af2229bef9d14e699b85f70
Matheus Valeriano <math.valeriano@gmail.com>
2019-12-24 12:56:33 -0300

    fix: position
    should use calc with window size

--- src/components/Hotspot/index.tsx
+++ src/components/Hotspot/index.tsx
@@ -3,16 +3,21 @@ import useOutsideClick from '@rooks/use-outside-click';
 import * as S from './styled';
 import HotspotInfo from '../HotspotInfo';
 import { Hotspot as Props } from 'shared/types/hotspot';
+import useResize from 'hooks/useResize';
 
 const Hotspot: React.FC<Props> = ({
   id,
   description,
   position,
   title,
+  windowSize = { offsetHeight: 0, offsetWidth: 0 },
 }: Props) => {
   const needToSetInfos = !title && !description;
   const [isShowingInfo, setIsShowingInfo] = useState(() => needToSetInfos);
   const ref = useRef<HTMLDivElement>(null);
+
+  const { width } = useResize();
+  const left = (position.left * width) / windowSize.offsetWidth;
 
   useOutsideClick(
     ref as MutableRefObject<HTMLDivElement>,
@@ -25,19 +30,19 @@ const Hotspot: React.FC<Props> = ({
   };
 
   return (
-    <>
+    <S.Wrapper left={left} top={position.top}>
       <S.Spot data-testid="spot" onClick={spotHandleClick} {...position} />
       {isShowingInfo && (
-        <article ref={ref}>
+        <S.InfoWrapper ref={ref}>
           <HotspotInfo
             id={id}
             title={title}
             description={description}
             position={position}
           />
-        </article>
+        </S.InfoWrapper>
       )}
-    </>
+    </S.Wrapper>
   );
 };
 
