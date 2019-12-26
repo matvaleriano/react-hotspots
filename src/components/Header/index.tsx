1cc63d2cf9224f2873e8535bcdcbadea48bf7a90
Matheus Valeriano <math.valeriano@gmail.com>
2019-12-26 15:28:42 -0300

    refactor: use map with link items

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,31 +4,38 @@ import { Github } from 'styled-icons/boxicons-logos/Github';
 import { LinkedinSquare } from 'styled-icons/boxicons-logos/LinkedinSquare';
 import { Twitter } from 'styled-icons/boxicons-logos/Twitter';
 
+const LINKS = [
+  {
+    href: 'https://github.com/mathvaleriano/react-hotspots',
+    icon: Github,
+    testId: 'github-link',
+  },
+  {
+    href: 'https://www.linkedin.com/in/mathvaleriano/',
+    icon: LinkedinSquare,
+    testId: 'linkedin-link',
+  },
+  {
+    href: 'https://twitter.com/mathvaleriano',
+    icon: Twitter,
+    testId: 'twitter-link',
+  },
+];
+
 const Header: React.FC = () => (
   <Flex fontSize={3} p={3} alignItems="center">
     <Box flex={'1 1 auto'}>
-      <Heading fontSize={[3, 4, 5]} as="h1">
+      <Heading fontSize={[3, 4, 5]} as="h1" data-testid="heading">
         Hotspots
       </Heading>
     </Box>
-    <Box px={1}>
-      <Link
-        href="https://github.com/mathvaleriano/react-hotspots"
-        target="_blank"
-      >
-        <Github style={{ width: '2rem', color: 'black' }} />
-      </Link>
-    </Box>
-    <Box px={1}>
-      <Link href="https://www.linkedin.com/in/mathvaleriano/" target="_blank">
-        <LinkedinSquare style={{ width: '2rem', color: 'black' }} />
-      </Link>
-    </Box>
-    <Box px={1}>
-      <Link href="https://twitter.com/mathvaleriano" target="_blank">
-        <Twitter style={{ width: '2rem', color: 'black' }} />
-      </Link>
-    </Box>
+    {LINKS.map(({ href, icon: Icon, testId }) => (
+      <Box px={1} key={testId}>
+        <Link data-testid={testId} href={href} target="_blank">
+          <Icon style={{ width: '2rem', color: 'black' }} />
+        </Link>
+      </Box>
+    ))}
   </Flex>
 );
 
